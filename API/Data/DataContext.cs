a31a6fbd376ee86403684d1ffd1f497d4854d824
Zach <zbbeaird1989@gmail.com>
2024-02-05 05:53:23 -0600

    Initial Commit

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using API.Entities;
+
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base()
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
